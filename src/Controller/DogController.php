88e6ed26d436cc0777ce563c50d39ea2ca05503d
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-04-27 12:41:42 +0200

    CRUD dog

---
+++ src/Controller/DogController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Dog;
+use App\Form\DogType;
+use App\Repository\DogRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/dog")
+ */
+class DogController extends AbstractController
+{
+    /**
+     * @Route("/", name="dog_index", methods={"GET"})
+     */
+    public function index(DogRepository $dogRepository): Response
+    {
+        return $this->render('dog/index.html.twig', [
+            'dogs' => $dogRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="dog_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $dog = new Dog();
+        $form = $this->createForm(DogType::class, $dog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($dog);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('dog_index');
+        }
+
+        return $this->render('dog/new.html.twig', [
+            'dog' => $dog,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="dog_show", methods={"GET"})
+     */
+    public function show(Dog $dog): Response
+    {
+        return $this->render('dog/show.html.twig', [
+            'dog' => $dog,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="dog_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Dog $dog): Response
+    {
+        $form = $this->createForm(DogType::class, $dog);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('dog_index');
+        }
+
+        return $this->render('dog/edit.html.twig', [
+            'dog' => $dog,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="dog_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Dog $dog): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$dog->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($dog);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('dog_index');
+    }
+}
---
+++ src/Form/DogType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Dog;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DogType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('cat')
+            ->add('price')
+            ->add('monkey')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Dog::class,
+        ]);
+    }
+}
