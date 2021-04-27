b5959a88dbe21fed7eb981b2919f0b741417f572
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-04-27 12:19:12 +0200

    migration class monkey on DB

---
+++ migrations/Version20210427101827.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210427101827 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE monkey (id INT AUTO_INCREMENT NOT NULL, monkey VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE saucisson (id INT AUTO_INCREMENT NOT NULL, sec VARCHAR(10) NOT NULL, price INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE dog ADD monkey VARCHAR(30) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE monkey');
+        $this->addSql('DROP TABLE saucisson');
+        $this->addSql('ALTER TABLE dog DROP monkey');
+    }
+}
