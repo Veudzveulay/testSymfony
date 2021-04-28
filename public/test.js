4a118d5aec42c138f685dee9bf43fd6d641ac381
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-04-28 12:57:49 +0200

    URL = appeler le ajaxcontroller.php

--- public/test.js
+++ public/test.js
@@ -1,6 +1,6 @@
 function ajax(data) {
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", '/ajax', true); // on va chercher le fichier php
+    xmlhttp.open("POST", '/ajax', true); // URL permet d'appele le ajaxControlle.php
     xmlhttp.send(data);
     xmlhttp.onload = response; //
 }
