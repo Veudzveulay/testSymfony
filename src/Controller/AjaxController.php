624d05ce7f55533f385ac6e6bcd4cd935f631af6
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-04-28 15:25:46 +0200

    URL = appeler le ajaxcontroller.php

---
+++ src/Controller/AjaxController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+/** use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+ * */
+
+class AjaxController extends AbstractController
+{
+    /**
+     * @Route("/ajax", name="ajax", methods = {"POST"} )
+     */
+    public function index() : JsonResponse {
+        return new JsonResponse("salut");
+
+    }
+
+}
