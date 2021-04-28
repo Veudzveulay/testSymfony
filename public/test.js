543299fdda30e0fd2c35f967ea9a04f64c7de589
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-04-28 16:52:05 +0200

    Test pour récuperer les valeurs dans le formulaire et mettre un alert

--- public/test.js
+++ public/test.js
@@ -8,4 +8,35 @@ function ajax(data) {
 function response() { // suite de la fonction ONLOAD
     console.log(this.responseText); // pour voir le texte dans la console
 }
+
+let button = document.getElementById('btn'); // prend la valeur du bouton
+
+button.addEventListener("click", function () {
+
+    if (validation_Nom_Prenom(document.getElementById("cat"))) {
+         var name = document.getElementById("cat");
+        alert ("$name");
+        const p = 1;
+
+    } else {
+        var name = document.getElementById("cat");
+        alert ("$name pas correct");
+    }
+    if (p = 1) {
+
+        let form = document.getElementById('table'); // on récupere tout les inputs du formulaire
+        let formData = new FormData(form);
+        ajax(formData); // on exécute la fonction ajax avec les données du formulaire
+
+        console.log(form); // affiche dans la console
+    }
+});
+
+
+function validation_Nom_Prenom(verif) {
+
+    let nom_Prenom = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u);
+    return nom_Prenom.test(verif);
+}
+
 ajax();
