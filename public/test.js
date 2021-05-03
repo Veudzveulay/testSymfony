9464853064cd19d7ed4b53290a9da5df8df84400
Veudzveulay <32245083+Veudzveulay@users.noreply.github.com>
2021-05-03 09:33:27 +0200

    Ajax avec vérification en BDD

--- public/test.js
+++ public/test.js
@@ -1,77 +1,73 @@
 let form = document.getElementById('myForm'); // prend la valeur du formulaire
-console.log(form);
+ console.log(form);
 
-function ajax(data) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", '/ajax', true); // URL permet d'appele le ajaxControlle.php
-    xmlhttp.send(data);
-    xmlhttp.onload = response; //
+async function ajax(data) {
+    return new Promise(resolve => {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST", '/ajax', true); // URL permet d'appele le ajaxControlle.php
+        xmlhttp.send(data);
+        xmlhttp.onload = function () {
+            resolve(this.responseText)
+        };
+    })
 }
 
-function response() { // suite de la fonction ONLOAD
-    console.log(this.responseText); // pour voir le texte dans la console
+function response() {
+    // console.log(this.responseText); // pour voir le texte dans la console
+    // document.getElementById("testEcrit").innerHTML = this.responseText;
+    return this.responseText;
 }
 
 
-/** function validation() {
-    if (form.document.getElementById('cat').value == "") {
-        verification.preventDefault();
-        alert("pas correct");
-
-
-    } else {
-
-        alert(" pas correct");
-    }
-    if () {
-
-
-        let formData = new FormData(form);
-        ajax(formData); // on exécute la fonction ajax avec les données du formulaire
-
-        console.log(form); // affiche dans la console
-    }
-}
- */
-
- function validation_Nom(verification) {
+function validation_Nom(verification) {
 
     let nom = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u);
     return nom.test(verification);
 }
+
 function validerEmail(email) {
     var mail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return mail.test(email);
 }
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault(); // empêche l'envoi sur formulaire
 
+    let formData = new FormData(form);
+    let catExists = await ajax(formData); // catExists attend que la fonction ajax soit fini avant de commencer
     let cat = form[0].value,
         price = form[1].value,
         monkey = form[2].value,
         error = 0,
         errorMsg = '';
 
-     // console.log(cat, price, monkey); // affiche dans la console le champ cat price monkey
+    // console.log(cat, price, monkey); // affiche dans la console le champ cat price monkey
+    // console.log(catExists); // typeof = montrer si c'est un string ou un int
+    // console.log(JSON.parse(catExists)); // typeof = montrer si c'est un string ou un int
 
-    if (!validation_Nom(cat)) {
-        errorMsg += "le nom de cat n'est pas correcte\r";
+    if (catExists === true) {
+        errorMsg += "Le nom est déjà pris"
         error++;
     }
-    if (!validation_Nom(monkey)) {
-        errorMsg += "le nom de monkey n'est pas correcte\r";
+    if (!validation_Nom(cat)) { // si la valeur dans le 2eme input ne passe pas le regex, msg d'erreur
+        errorMsg += "le nom de cat n'est pas correcte \r";
         error++;
     }
-    // console.log(error);  affiche les nombres d'erreurs qu'il y a
+    if (!validation_Nom(monkey)) { // si la valeur dans le 3eme input ne passe pas le regex, un msg d'erreur est formé
+        // + une incrémentation
+        errorMsg += "le nom de monkey n'est pas correcte \r";
+        error++;
+    }
+    //console.log(error);  //affiche les nombres d'erreurs qu'il y a
+    //console.log(errorMsg); // affiche errorMsg dans la console
 
     if (error > 0) {
-        document.getElementById("testEcrit").innerHTML = errorMsg;
-    }
-    else {
-        let formData = new FormData(form);
-        ajax(formData); // on exécute la fonction ajax avec les données du formulaire
-       // console.log(form);  affiche dans la console
+        document.getElementById("testEcrit").innerHTML = errorMsg; // si il y a plus de une erreur envoie la
+        // fonction errorMsg
+
+    } else {
+         form.submit(); // envoie le formulaire
+       //  console.log(form);  // affiche dans la console
     }
 });
 
--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Dog;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,12 +15,41 @@ class AjaxController extends AbstractController
      * @Route("/ajax", name="ajax", methods = {"POST"} )
      */
 
-      public function index() : JsonResponse {
-          $cat = $_POST["dog"]["cat"];
-         // $name = $_POST["username"];
-          var_dump($cat);
-          return new JsonResponse($cat, 200);
-    }
+    public function index(): JsonResponse
+    {
+        $cat = $_POST["dog"]["cat"]; // permet de récuperer ce qu'il y a dans l'input dog_cat
+        $filtered_cat_name = filter_var($cat, FILTER_SANITIZE_STRING);
+        $monkey = $_POST["dog"]["monkey"]; // permet de récuperer ce qu'il y a dans l'input dog_monkey
+        $filtered_monkey_name = filter_var($monkey, FILTER_SANITIZE_STRING);
 
+        $repository = $this->getDoctrine()
+            ->getRepository(Dog::class);
+
+        $dog_cat = $repository->findOneBy(array('cat' => $filtered_cat_name)); // on va cherche dans le repository tout les noms qui sont comme $cat
+        $dog_monkey = $repository->findOneBy(array('monkey' => $filtered_monkey_name)); // on va cherche dans le repository
+        // tout les noms qui sont comme $monkey
+
+        $error_msg = [];
+
+        if (!empty($dog_cat)) {
+            array_push($error_msg, 'Cat exists');
+        }
+        if (!empty($dog_monkey)) {
+            array_push($error_msg, 'Monkey exists.');
+        }
+        if (!empty($error_msg)) {
+            $response = [
+                'error' => true,
+                'msg' => $error_msg
+            ];
+            return new JsonResponse($response);
+        }
+
+        if (empty($dog_cat)) {
+            return new JsonResponse(false);
+        } else {
+            return new JsonResponse(true);
+        }
+    }
 
 }
